a05abdd2e5974c0cd4590ae00d7f8130416eb23f
mockenoff <mockenoff@yahoo.com>
2015-04-11 20:36:06 -0500

    Stand up app with Parse integrations

---
+++ Hop/Hop-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Hop-Bridging-Header.h
+//  Hop
+//
+//  Created by Tim Poon on 4/11/15.
+//  Copyright (c) 2015 Hop. All rights reserved.
+//
+
+#ifndef Hop_Bridging_Header_h
+#define Hop_Bridging_Header_h
+
+#import <Parse/Parse.h>
+#import <Bolts/Bolts.h>
+
+#endif
---
+++ Hop/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hop
+//
+//  Created by Tim Poon on 4/11/15.
+//  Copyright (c) 2015 Hop. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
